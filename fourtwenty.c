acb2f49c50da65a8888bee1decc90bc79afa6310
Victor Wang <37973874+vyzw@users.noreply.github.com>
2020-10-04 21:12:17 -0700

    initial commit

---
+++ fourtwenty.c
@@ -0,0 +1,10 @@
+int main(void) {
+  int fourtwenty = 0;
+  for (int i = 0; i <= 20; i++) {
+      int twenty = 0;
+      for (int j = 0; j <= 4; j++) {
+          twenty += 4;
+      }
+      fourtwenty += twenty;
+  }
+}
